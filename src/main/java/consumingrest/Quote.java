f5ee90f1de39f183b137f679a97164c3ad445eb7
Morenzo Palmer <moreenzop@gmail.com>
2019-10-24 10:32:25 -0400

    1st

---
+++ src/main/java/consumingrest/Quote.java
@@ -0,0 +1,36 @@
+package consumingrest;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jdk.internal.org.objectweb.asm.tree.analysis.Value;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Quote {
+    private String type;
+    private Value value;
+
+    public Quote(){
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Value getValue() {
+        return value;
+    }
+
+    public void setValue(Value value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Quote{" +
+                "type='" + type + '\'' +
+                ", value=" + value +
+                '}';
+    }
+}
